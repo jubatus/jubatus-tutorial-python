cd25457debdb14a79600435fba449079b215fc74
ODA Satoshi <oda.satoshi@gmail.com>
2012-05-13 11:03:58 +0900

    fix tutorial.py for latest python client

--- tutorial.py
+++ tutorial.py
@@ -2,13 +2,16 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import jubatus
+from jubatus.classifier import client
+from jubatus.classifier import types
 
 def parse_args():
     from optparse import OptionParser, OptionValueError
     p = OptionParser()
-    p.add_option('-s', '--server_list', action='store',
-                 dest='server_list', type='string', default='localhost:9199')
+    p.add_option('-s', '--server_ip', action='store',
+                 dest='server_ip', type='string', default='127.0.0.1')
+    p.add_option('-p', '--server_port', action='store',
+                 dest='server_port', type='int', default='9199')
     p.add_option('-n', '--name', action='store',
                  dest='name', type='string', default='test')
     p.add_option('-a', '--algo', action='store',
@@ -22,9 +25,9 @@ def get_most_likely(estm):
     result[0] = ''
     result[1] = 0
     for res in estm:
-        if prob == None or res[1] > prob :
-            ans = res[0]
-            prob = res[1]
+        if prob == None or res.prob > prob :
+            ans = res.label
+            prob = res.prob
             result[0] = ans
             result[1] = prob
     return result
@@ -34,67 +37,58 @@ def get_most_likely(estm):
 if __name__ == '__main__':
     options, remainder = parse_args()
 
-    classifier = jubatus.Classifier(options.server_list, options.name)
+    classifier = client.classifier(options.server_ip,options.server_port)
 
-    config = {
-            'converter': {
-              "string_filter_types": {
-              "detag": { "method": "regexp", "pattern": "<[^>]*>", "replace": "" }
-               },
-              "string_filter_rules":
-                 [
-                { "key": "message", "type": "detag", "suffix": "-detagged" }
-                 ],
-                'num_filter_types': {},
-                'num_filter_rules': [],
-                'string_types': {},
-                'string_rules': [
-                    {'key': 'message-detagged', 'type': "space", "sample_weight": "bin", "global_weight": "bin"}
-                    ],
-                'num_types': {},
-                'num_rules': []
-                },
-            'method': options.algo
-            }
+    str_fil_types = {"detag": {"method": "regexp", "pattern": "<[^>]*>", "replace": "" }}
+    str_fil_rules = [types.filter_rule("message", "detag", "-detagged")]
+    num_fil_types = {}
+    num_fil_rules = []
+    str_type= {}
+    str_rules = [types.string_rule("message-detagged","space","bin","bin")]
+    num_type = {}
+    num_rules = []
 
-    print classifier.set_config(config)
+    converter = types.converter_config(str_fil_types, str_fil_rules, num_fil_types, num_fil_rules,
+                                       str_type, str_rules, num_type, num_rules)
+    config = types.config_data(options.algo, converter);
+
+    print config
+
+    pname = options.name
+
+    print classifier.set_config(pname,config)
     
-    print classifier.get_config()
+    print classifier.get_config(pname)
+    print classifier.get_status(pname)
 
-    print classifier.get_status()
 
-    i = 0
     for line in open('train.dat'):
         label, file = line[:-1].split(',')
         dat = open(file).read()
-        classifier.train(
-            [( label ,  ([["message", dat]], ) ,)]
-        )
-        i = i + 2
-        print i, classifier.get_status()
+        datum = types.datum(  [["message", dat]], [] )
+#                           ([sv(=string vector)], [nv(=number vector)])
+        classifier.train(pname,[(label,datum)])
+        print classifier.get_status(pname)
 
-    print classifier.save("tutorial")
+    print classifier.save(pname, "tutorial")
 
-    print classifier.load("tutorial")
+    print classifier.load(pname, "tutorial")
 
-    print classifier.set_config(config)
+    print classifier.set_config(pname, config)
 
-    print classifier.get_config()
+    print classifier.get_config(pname)
 
     for line in open('test.dat'):
         label, file = line[:-1].split(',')
         dat = open(file).read()        
-        ans = classifier.classify(
-            [([["message", dat]], )]
-           )
-        print ans
-        i = i + 1
+        datum = types.datum(  [["message", dat]], [] )
+        ans = classifier.classify(pname,[(datum)])
         if ans != None:
             estm = get_most_likely(ans[0])
             if (label == estm[0]):
                 result = "OK"
             else:
                 result = "NG"
-            print i, result + "," + label + ", " + estm[0] + ", " + str(estm[1])
+            print result + "," + label + ", " + estm[0] + ", " + str(estm[1])
 
 
